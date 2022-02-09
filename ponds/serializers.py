8d78f34727d17b52bcc7f2db31cb453187cfa9b4
ext23 <xay345@gmail.com>
2022-02-09 18:01:59 +0300

    sectors added

--- ponds/serializers.py
+++ ponds/serializers.py
@@ -1,12 +1,24 @@
 from rest_framework import serializers
-from .models import Pond
+from .models import Pond, Sector
 
 
 class PondSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Pond
+        sectors = serializers.SlugRelatedField(
+            many=True,
+            read_only=True,
+            slug_field='number',
+            allow_null=True,
+            required=False)
+
         fields = ('id', 'name', 'zone', 'width_bottom', 'width_top',
                   'max_depth', 'avg_depth', 'square', 'fish_density',
                   'avg_fish_weight', 'description', 'logo', 'address',
-                  'width', 'sector_width', 'cell_height')
+                  'width', 'sector_width', 'cell_height', 'sectors')
+
+
+class SectorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Sector
+        fields = ('pond', 'number')
--- ponds/urls.py
+++ ponds/urls.py
@@ -1,7 +1,8 @@
 from rest_framework.routers import SimpleRouter
-from .views import PondViewSet
+from .views import PondViewSet, SectorViewSet
 
 router = SimpleRouter()
 router.register('ponds', PondViewSet, basename='ponds')
+router.register('sectors', SectorViewSet, basename='sectors')
 
 urlpatterns = router.urls
